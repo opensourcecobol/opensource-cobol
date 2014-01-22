4f3d1201bb3e5b862ec44e203021ba9a016cf0ab
Kazuyoshi TANAKA <opencobol@kzta.net>
2014-01-22 07:20:39 +0000

    #115 [CE:r53] backport from OpenCOBOL CE1.1

--- libcob/fileio.c
+++ libcob/fileio.c
@@ -3976,7 +3976,11 @@ cob_ex_open (cob_file *f, const int mode, const int sharing, cob_field *fnstatus
 	}
 
 	/* obtain the file name */
-	cob_field_to_string (f->assign, file_open_name);
+	if(f->assign == NULL) {
+		strncpy (file_open_name, f->select_name, COB_SMALL_MAX);
+	} else {
+		cob_field_to_string (f->assign, file_open_name);
+	}
 
 #ifdef	WITH_INDEX_EXTFH
 	if (f->organization == COB_ORG_INDEXED) {
@@ -4942,7 +4946,11 @@ cob_exit_fileio (void)
 	for (l = file_cache; l; l = l->next) {
 		if (l->file->open_mode != COB_OPEN_CLOSED &&
 		     l->file->open_mode != COB_OPEN_LOCKED) {
-			cob_field_to_string (l->file->assign, runtime_buffer);
+			if(l->file->assign == NULL) {
+				strncpy (runtime_buffer, l->file->select_name, COB_SMALL_MAX);
+			} else {
+				cob_field_to_string (l->file->assign, runtime_buffer);
+			}
 			cob_close (l->file, 0, NULL);
 			str_logic_filename = cb_get_jisword (l->file->select_name);
 			str_physical_filename = cb_get_jisword (runtime_buffer);
