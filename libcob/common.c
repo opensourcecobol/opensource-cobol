2a59c8a198140ea9cede76588d391ea1fd287afb
Noriyuki Isaka <n-isaka@tsh-world.co.jp>
2016-07-01 15:09:32 +0900

    constが利用できない個所で使用していたため、削除

--- libcob/common.c
+++ libcob/common.c
@@ -2685,7 +2685,7 @@ cob_acuw_calledby (unsigned char *data)
 			memset (f1->data, ' ', (int)f1->size);
 			return 0;
 		} else {
-			called_program_name = (const char *)cob_current_module->next->program_id;
+			called_program_name = (char *)cob_current_module->next->program_id;
 			if (called_program_name == NULL) {
 				return -1;
 			}
